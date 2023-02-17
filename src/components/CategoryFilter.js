f4596d85543174f821bf7d20d500b680cec00ef5
damilolaakinbi <akinbidamilola074@gmail.com>
2023-02-17 08:41:16 +0100

    started CategoryFilter but not done

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CategoryFilter() {
+function CategoryFilter({ categories }) {
+  const [isSelected, setIsSelected] = useState(false);
+  // console.log("CategoryFilter", categories);
+
+  const allCategories = categories.map((member) => (
+    <button
+      key={member}
+      // onClick={() => handleBtnClick(member)}
+      onClick={handleBtnClick}
+    >
+      {member}
+    </button>
+  ));
+
+  function handleBtnClick(event) {
+    // console.log(allCategories[4].key);
+    // console.log(event.target);
+    // console.log("className", event.target.className);
+    if (event.target.className === "") event.target.classList.add("selected");
+    else if (event.target.className === "selected")
+      event.target.classList.remove("selected");
+    setIsSelected((isSelected) => !isSelected);
+  }
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
       {/* render <button> elements for each category here */}
+      {allCategories}
     </div>
   );
 }
