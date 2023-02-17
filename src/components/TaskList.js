e6ecbfa4aefdab1d9d17affb903761b29e4559f7
damilolaakinbi <akinbidamilola074@gmail.com>
2023-02-17 06:22:13 +0100

    built the TaskList, partially build Task functionality

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,17 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({ tasks }) {
+  // console.log("TaskList", tasks);
+  const allTasks = tasks.map((member, index) => {
+    return <Task key={index} text={member.text} category={member.category} />;
+  });
+  // console.log(allTasks);
+
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
+      {allTasks}
     </div>
   );
 }
